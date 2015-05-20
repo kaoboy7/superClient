37e8f18f29fbf0549951838092eb80651cdbfe8d
alex <alexkao7@gmail.com>
2015-05-19 21:58:35 -0400

    added Procfile and misc

--- Gruntfile.js
+++ Gruntfile.js
@@ -457,6 +457,10 @@ module.exports = function (grunt) {
     ]);
   });
 
+  grunt.registerTask('heroku', 
+    ['compass:dist', 'autoprefixer', 'imagemin']);
+
+
   grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
     grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
     grunt.task.run(['serve:' + target]);
