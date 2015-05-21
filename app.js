0c7f2aea0c4c2f206029282d2023777771326b42
alex <alexkao7@gmail.com>
2015-05-20 22:10:06 -0400

    preparing for heroku depoy

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+var gzippo = require('gzippo');
 var express = require('express');
+var morgan = require('morgan');
+
 var port = process.env.PORT || 3000;
+
 var app = express();
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
 
-app.use(express.static(__dirname + '/public'));
+
+
+// app.use(express.static(__dirname + '/public'));
 
 app.listen(port);
